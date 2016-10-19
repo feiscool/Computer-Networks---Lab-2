c328a23338ecf6cf72311f39e9a62cc8b45bca41
Frank Edwards <feiscool@users.noreply.github.com>
2016-10-19 13:32:46 -0500

    Improved print statements

--- Master.py
+++ Master.py
@@ -16,17 +16,17 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 portNum = int(sys.argv[1])
 # Bind the socket to the port
 server_address = ('', portNum)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+print('Master: Starting up on port #%s' % server_address)
 sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
 
 while True:
     # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
+    print('Master: Waiting for a connection')
     connection, client_address = sock.accept()
     try:	# Receive the data in small chunks and retransmit it
-    	print >>sys.stderr, 'connection from', client_address
+    	print('Master: Received connection from %s' % client_address)
     	data = connection.recv(16)
     	dataCharArray = list(binascii.hexlify(data))
     	#GIDReceived is the GID of the creator or the slave
@@ -35,12 +35,12 @@ while True:
     	#ignore the request if the message is not valid (different from 3 bytes or not containing the magic number).
     	magicNumber = str(dataCharArray[2]) + str(dataCharArray[3]) + str(dataCharArray[4]) + str(dataCharArray[5])
 
-    	print >>sys.stderr, 'GIDReceived', GIDReceived
-    	print >>sys.stderr, 'magicNumber', magicNumber
+    	print('Master: GIDReceived = %s' % GIDReceived)
+    	print('Master: magicNumber = %s' % magicNumber)
     
     	#reply = struct.pack("=bhbI", myGID, magicNumber, myRID, nextSlaveIP
     	# if reply:
-    	# 	print >>sys.stderr, 'sending data back to the client\n'
+    	# 	print('Master: Sending data back to the client')
     	# 	connection.sendall(reply)
     	# else: #no more data to read
     	# 	break		
