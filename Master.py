bac49055f0bea213605740d356072333b7dcd8b4
Savannah Forood <sforood@gmail.com>
2016-10-20 17:30:04 -0500

    Error in sending bytearray

--- Master.py
+++ Master.py
@@ -1,17 +1,18 @@
 import socket
 import sys
+import string
 import struct
 import binascii
-
-		#when a node wants to join, 
-		#assign it a ring ID myRID 
-		#and the IP address of their successor node (nextSlaveIP)
-
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 # Create a TCP/IP socket
 myRID = 0
 myGID = 1
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#both IPaddress and nextSlaveIP are initially set to the master IP
+IPaddress = socket.gethostbyname(socket.gethostname())
+nextSlaveIP = IPaddress
 #get port number from argv
 portNum = int(sys.argv[1])
 # Bind the socket to the port
@@ -25,34 +26,46 @@ while True:
     # Wait for a connection
     print 'Master: Waiting for a connection'
     connection, client_address = sock.accept()
-    try:	# Receive the data in small chunks and retransmit it
-    	print 'Master: Received connection from', client_address
-    	
-	client_IP = client_address[0] 
-	client_Port = client_address[1]
-	print 'Master: IP address of slave =', client_IP
-	print 'Master: Port number of slave =', client_Port
+    try:    # Receive the data in small chunks and retransmit it
+        print 'Master: Received connection from', client_address
+        
+        client_IP = client_address[0] 
+        client_Port = client_address[1]
+        print 'Master: IP address of slave =', client_IP
+        print 'Master: Port number of slave =', client_Port
 
-	data = connection.recv(16)
-    	dataCharArray = list(binascii.hexlify(data))
-    	#GIDReceived is the GID of the creator or the slave
-    	GIDReceived_String = str(dataCharArray[0]) + str(dataCharArray[1])
-    	#magicNumber is used by the nodes to test the validity of messages using this protocol
-    	#ignore the request if the message is not valid (different from 3 bytes or not containing the magic number).
-    	magicNumber_String = str(dataCharArray[4]) + str(dataCharArray[5]) + str(dataCharArray[2]) + str(dataCharArray[3])
+        #IPaddress(master IP) is assigned to the newly received node IP
+        nextSlaveIP = IPaddress
+        IPaddress = client_IP
 
-	GIDReceived = int(GIDReceived_String)
-	magicNumber = int(magicNumber_String)
+        data = connection.recv(16)
+        dataCharArray = list(binascii.hexlify(data))
+        #GIDReceived is the GID of the creator or the slave
+        GIDReceived_String = str(dataCharArray[0]) + str(dataCharArray[1])
+        #magicNumber is used by the nodes to test the validity of messages using this protocol
+        #ignore the request if the message is not valid (different from 3 bytes or not containing the magic number).
+        magicNumber_String = str(dataCharArray[4]) + str(dataCharArray[5]) + str(dataCharArray[2]) + str(dataCharArray[3])
 
-    	print 'Master: GIDReceived =', GIDReceived
-    	print 'Master: magicNumber =', magicNumber
-    
-    	#reply = struct.pack("=bhbI", myGID, magicNumber, myRID, nextSlaveIP
-    	# if reply:
-    	# 	print 'Master: Sending data back to the client'
-    	# 	connection.sendall(reply)
-    	# else: #no more data to read
-    	# 	break		
+        GIDReceived = int(GIDReceived_String)
+        magicNumber = int(magicNumber_String)
+
+        print 'Master: GIDReceived =', GIDReceived
+        print 'Master: magicNumber =', magicNumber
+
+        #print 'nextSlaveIP text to binary', binascii.hexlify(socket.inet_aton(nextSlaveIP))
+        nextSlaveIPArray = bytearray()
+        nextSlaveIPArray.extend(nextSlaveIP)
+        print nextSlaveIPArray
+
+        reply = struct.pack("=bhb", myGID, magicNumber, myRID, nextSlaveIPArray)
+
+        if reply:
+            print 'Master: Sending data back to the client'
+            connection.sendall(reply)
+            print 'Master: Sent %s bytes back to %s' % (len(reply), client_IP)
+
+        else: #no more data to read
+          break       
          
  
             
