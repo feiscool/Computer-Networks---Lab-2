82db2520d34ea5859a59b7996beb7b6a6893d328
Frank Edwards <feiscool@users.noreply.github.com>
2016-10-20 08:18:29 -0500

    Got Slave hostname and port number separately

--- Master.py
+++ Master.py
@@ -3,9 +3,9 @@ import sys
 import struct
 import binascii
 
-                #when a node wants to join, 
-                #assign it a ring ID myRID 
-                #and the IP address of their successor node (nextSlaveIP)
+		#when a node wants to join, 
+		#assign it a ring ID myRID 
+		#and the IP address of their successor node (nextSlaveIP)
 
 
 # Create a TCP/IP socket
@@ -25,31 +25,37 @@ while True:
     # Wait for a connection
     print 'Master: Waiting for a connection'
     connection, client_address = sock.accept()
-    try:        # Receive the data in small chunks and retransmit it
-        print 'Master: Received connection from', client_address
-        data = connection.recv(16)
-        dataCharArray = list(binascii.hexlify(data))
-        #GIDReceived is the GID of the creator or the slave
-        GIDReceived_String = str(dataCharArray[0]) + str(dataCharArray[1])
-        #magicNumber is used by the nodes to test the validity of messages using this protocol
-        #ignore the request if the message is not valid (different from 3 bytes or not containing the magic number).
-        magicNumber_String = str(dataCharArray[4]) + str(dataCharArray[5]) + str(dataCharArray[2]) + str(dataCharArray[3])
+    try:	# Receive the data in small chunks and retransmit it
+    	print 'Master: Received connection from', client_address
+    	
+	client_IP = client_address[0] 
+	client_Port = client_address[1]
+	print 'Master: IP address of slave =', client_IP
+	print 'Master: Port number of slave =', client_Port
 
-        GIDReceived = int(GIDReceived_String)
-        magicNumber = int(magicNumber_String)
+	data = connection.recv(16)
+    	dataCharArray = list(binascii.hexlify(data))
+    	#GIDReceived is the GID of the creator or the slave
+    	GIDReceived_String = str(dataCharArray[0]) + str(dataCharArray[1])
+    	#magicNumber is used by the nodes to test the validity of messages using this protocol
+    	#ignore the request if the message is not valid (different from 3 bytes or not containing the magic number).
+    	magicNumber_String = str(dataCharArray[4]) + str(dataCharArray[5]) + str(dataCharArray[2]) + str(dataCharArray[3])
 
-        print 'Master: GIDReceived =', GIDReceived
-        print 'Master: magicNumber =', magicNumber
+	GIDReceived = int(GIDReceived_String)
+	magicNumber = int(magicNumber_String)
 
-        #reply = struct.pack("=bhbI", myGID, magicNumber, myRID, nextSlaveIP
-        # if reply:
-        #       print 'Master: Sending data back to the client'
-        #       connection.sendall(reply)
-        # else: #no more data to read
-        #       break           
-
-
-
+    	print 'Master: GIDReceived =', GIDReceived
+    	print 'Master: magicNumber =', magicNumber
+    
+    	#reply = struct.pack("=bhbI", myGID, magicNumber, myRID, nextSlaveIP
+    	# if reply:
+    	# 	print 'Master: Sending data back to the client'
+    	# 	connection.sendall(reply)
+    	# else: #no more data to read
+    	# 	break		
+         
+ 
+            
     finally:
         # Clean up the connection
-        connection.close()                         
+        connection.close()
