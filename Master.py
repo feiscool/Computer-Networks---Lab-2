912aaeaa2fd2c425a2483b92ad1a3a2d0bb2b6fb
Savannah Forood <sforood@gmail.com>
2016-10-23 17:21:19 -0500

    IP address assignment loop, retrieving magic number, increment myRID,
    IP address assignment logic is explained in the comments; myRID
    increments on each joining node; magic number is retrieved correctly; I
    tested by creating slaves on two machines simultaneously and the
    nextSlaveIPs are being assigned correctly.

--- Master.py
+++ Master.py
@@ -7,11 +7,13 @@ import binascii
 # Create a TCP/IP socket
 myRID = 0
 myGID = 1
-magicNumber = 0x1234 
+magic_number = 0x1234
+packed_clientIP = -1
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-myIPaddress = socket.gethostbyname(socket.gethostname())
+#initially, both IP address variables are set to hostname
+#because the master is the only node in the ring
+myIPaddress = nextSlaveIP = socket.gethostbyname(socket.gethostname())
 
 #get port number from argv
 portNum = int(sys.argv[1])
@@ -28,47 +30,45 @@ while True:
     connection, client_address = sock.accept()
     try:    # Receive the data in small chunks and retransmit it
         print 'Master: Received connection from', client_address
-        
-        client_IP = client_address[0] 
+
+        client_IP = client_address[0]
         client_Port = client_address[1]
         print 'Master: IP address of slave =', client_IP
         print 'Master: Port number of slave =', client_Port
 
-        #IPaddress(master IP) is assigned to the newly received node IP
-        nextSlaveIP = myIPaddress
-
         data = connection.recv(16)
         dataCharArray = list(binascii.hexlify(data))
-        #GIDReceived is the GID of the creator or the slave
-        GIDReceived = str(dataCharArray[0]) + str(dataCharArray[1])
-        #magicNumber is used by the nodes to test the validity of messages using this protocol
+        #GID_received is the GID of the creator or the slave
+        GID_received = str(dataCharArray[0]) + str(dataCharArray[1])
+        #magic_number is used by the nodes to test the validity of messages using this protocol
         #ignore the request if the message is not valid (different from 3 bytes or not containing the magic number).
-        received_magicNumber = str(dataCharArray[2]) + str(dataCharArray[3]) + str(dataCharArray[4]) + str(dataCharArray[5]) 
+        received_magic_number = str(dataCharArray[2]) + str(dataCharArray[3]) + str(dataCharArray[4]) + str(dataCharArray[5])
 
-	GIDReceived = int(GIDReceived)
-	received_magicNumber = int(received_magicNumber, 16)	
+        GID_received = int(GID_received)
+        received_magic_number = int(received_magic_number, 16)
 
-	print 'Master: GIDReceived =', GIDReceived
-        print 'Master: magicNumber = 0x%x' % received_magicNumber
+        print 'Master: GID_received =', GID_received
+        print 'Master: magic_number = 0x%x' % received_magic_number
 
-	if received_magicNumber != magicNumber:
-		print 'Master: Error - invalid magic number received from Slave'
-		exit(1)  
+        if received_magic_number != magic_number:
+            print 'Master: Error - invalid magic number received from Slave'
+            exit(1)
 
-	packed_clientIP = struct.unpack("I", socket.inet_aton(nextSlaveIP))[0] 
-
-        reply = struct.pack("!BHBI", myGID, magicNumber, myRID, packed_clientIP)
+        #master has received and unpacked the request from the slave,
+        #so we do the following:
+        #---send myIPaddress to slave
+        #---set myIPaddress to the newly received IP address
+        packed_clientIP = struct.unpack("I", socket.inet_aton(myIPaddress))[0]
+        reply = struct.pack("!BHBI", myGID, magic_number, myRID, packed_clientIP)
+        nextSlaveIP = myIPaddress
+        myIPaddress = client_IP
+        myRID = myRID + 1
 
         if reply:
             print 'Master: Sending data back to the client'
             connection.sendall(reply)
             print 'Master: Sent %s bytes back to %s' % (len(reply), client_IP)
 
-        else: #no more data to read
-          break       
-         
- 
-            
     finally:
         # Clean up the connection
         connection.close()
--- Slave.c
+++ Slave.c
@@ -1,19 +1,19 @@
-/* 
+/*
  * - File name: Slave.c
  * - Command line argument format: Slave MasterHostname MasterPortNumber
  *   (where "Slave" is the executable)
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>	
-#include <sys/socket.h>	
-#include <netinet/in.h>		
-#include <netdb.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-#define MAXDATASIZE 100 // Max number of bytes we can get at once 
+#define MAXDATASIZE 100 // Max number of bytes we can get at once
 
 // Get sockaddr, IPv4 or IPv6
 void *get_in_addr(struct sockaddr *sa) {
@@ -30,50 +30,50 @@ int main(int argc, char *argv[]) {
 	// Stuff already added by Beej
     int sockfd, numbytes, rv, masterPortNumber;
     char buffer[MAXDATASIZE];
-    struct addrinfo hints, *servinfo, *p;	// "hints" is a struct of the addrinfo type 
+    struct addrinfo hints, *servinfo, *p;	// "hints" is a struct of the addrinfo type
     char s[INET6_ADDRSTRLEN];
-    
-    // Constants 
-    const uint8_t GID = 1;	
-    const uint16_t magicNumber = 0x1234;  
-    const uint16_t magicNumber_BigE = htons(magicNumber);		// Convert to Big Endian 
-    
-    // Variables to be received from the master 
+
+    // Constants
+    const uint8_t GID = 1;
+    const uint16_t magicNumber = 0x1234;
+    const uint16_t magicNumber_BigE = htons(magicNumber);		// Convert to Big Endian
+
+    // Variables to be received from the master
     uint8_t received_GID;
     uint16_t received_MagicNumber;
     uint8_t received_myRID;
     uint32_t received_nextSlaveIP;
     uint8_t myRID;
     uint32_t nextSlaveIP;
-    
+
     char nextSlaveIP_String[INET_ADDRSTRLEN];
-    
+
     // Packed struct that will be sent as the data in a packet. A packed struct
     // is used as it won't contain any "padding" added by the compiler
-    struct packed_message {   
-    	uint8_t GID_Struct; 
-    	uint16_t magicNumber_Struct;   
+    struct packed_message {
+    	uint8_t GID_Struct;
+    	uint16_t magicNumber_Struct;
     } __attribute__((__packed__));
-    
-    // Ensure enough command line arguments are given 
+
+    // Ensure enough command line arguments are given
     if (argc != 3) {
         fprintf(stderr,"Slave: Error - inappropriate amount of arguments given \n");
         exit(1);
     }
-    
+
     masterPortNumber = strtol(argv[2], NULL, 10);
-    
-    // Ensure the port number is valid (just for kicks) 
+
+    // Ensure the port number is valid (just for kicks)
     if (masterPortNumber < 0 || masterPortNumber > 65535) {
     	fprintf(stderr,"Slave: Error - invalid port number given \n");
         exit(1);
     }
 
 	// The "hints" struct is used in the call to getaddrinfo(). It gives hints about
-	// the type of socket we will be using 
-    memset(&hints, 0, sizeof hints);	// Ensure the struct is empty 
+	// the type of socket we will be using
+    memset(&hints, 0, sizeof hints);	// Ensure the struct is empty
     hints.ai_family = AF_UNSPEC;		// No preference for IPv4 or IPv6
-    hints.ai_socktype = SOCK_STREAM;	// We're using TCP, not UDP! 
+    hints.ai_socktype = SOCK_STREAM;	// We're using TCP, not UDP!
 
 	// Creates a linked list of addrinfo structs, which are pointed to by servinfo. These
 	// structs contain the address information for the server that we are connecting to
@@ -84,12 +84,12 @@ int main(int argc, char *argv[]) {
 
     // Loop through the linked list pointed to by servinfo until an addrinfo node is
     // found that can both create a socket and establish a connection. A loop is used
-    // as a host can have multiple addresses - not all may work 
+    // as a host can have multiple addresses - not all may work
     for(p = servinfo; p != NULL; p = p -> ai_next) {
-    
+
     	// Attempt to create a socket using the current addrinfo node. If the call
-    	// fails, jump to the next node in the iteration and try again. If succesful, 
-    	// try to connect to the host (the master) 
+    	// fails, jump to the next node in the iteration and try again. If succesful,
+    	// try to connect to the host (the master)
         if ((sockfd = socket(p -> ai_family, p -> ai_socktype,
                 p -> ai_protocol)) == -1) {
             perror("Slave: Error - socket() \n");
@@ -102,8 +102,8 @@ int main(int argc, char *argv[]) {
             perror("Slave: Error - connect() \n");
             continue;
         }
-        
-        break;	
+
+        break;
     }
 
 	// If 'p' is NULL at this point, then the entire linked list was iterated over and
@@ -113,70 +113,71 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
-	// Convert the IP address that 'p' contains to human readable form and display it 
+	// Convert the IP address that 'p' contains to human readable form and display it
     inet_ntop(p -> ai_family, get_in_addr((struct sockaddr *)p -> ai_addr), s, sizeof s);
     printf("Slave: Connecting to %s \n", s);
 
-	/* 
+	/*
 	 *
 	 * JOIN REQUEST is below
 	 *
 	 */
 
-	// Construct the packet to be sent 
+	// Construct the packet to be sent
     struct packed_message packet = {GID, magicNumber_BigE};
-    
-    // Attempt to send the packet to the master 
+
+    // Attempt to send the packet to the master
     if (send(sockfd, (void *)&packet, sizeof(packet), 0) == -1) {
     	perror("Slave: Error - send() \n");
 		close(sockfd);
 		exit(1);
     }
-    
-    // Receive the response from the master 
+
+    // Receive the response from the master
     if ((numbytes = recv(sockfd, buffer, MAXDATASIZE - 1, 0)) == -1) {
 	    perror("Slave: Error - recv() \n");
 	    exit(1);
 	}
-	
-	printf("Slave: Number of bytes received = %d \n", numbytes);	// REMOVE 
-	
-	// Ensure the return packet is the correct size (8 bytes) 
+
+	printf("Slave: Number of bytes received = %d \n", numbytes);	// REMOVE
+
+	// Ensure the return packet is the correct size (8 bytes)
 	if (numbytes != 8) {
 		perror("Slave: Error - incorrect response packet size \n");
 		exit(1);
 	}
 
 	buffer[numbytes] = '\0';		// Mark the end of the buffer
-	
+
 	// Get the data from the buffer. Note: The second parentheses is where the data
 	// is to begin to be parsed from. It will stop automatically based on the size of
-	// the type that is specified 
-	received_GID = *(uint8_t *)(buffer);		
+	// the type that is specified
+	received_GID = *(uint8_t *)(buffer);
 	received_MagicNumber = *(uint16_t *)(buffer + 1);
-    myRID = *(uint8_t *)(buffer + 3);
-    nextSlaveIP = *(uint32_t *)(buffer + 4);
-    
+  received_myRID = *(uint8_t *)(buffer + 3);
+  nextSlaveIP = *(uint32_t *)(buffer + 4);
+
     // Convert the received values larger than a byte to host byte order (Little Endian)
-    received_MagicNumber = ntohs(received_MagicNumber);		
+    received_MagicNumber = ntohs(received_MagicNumber);
     nextSlaveIP = ntohl(nextSlaveIP);
-    
-    // Convert the received next slave's IP Address to human readable form 
+
+    // Convert the received next slave's IP Address to human readable form
     inet_ntop(AF_INET, &(nextSlaveIP), nextSlaveIP_String, INET_ADDRSTRLEN);
-    
-    // Ensure that the magic number sent from the Master is valid 
+
+    // Ensure that the magic number sent from the Master is valid
     if (received_MagicNumber != magicNumber) {
     	perror("Slave: Error - invalid magic number received from Master \n");
-		exit(1);
+		    exit(1);
     }
-	
+
 	printf("Slave: GID of Master = %d \n", received_GID);
 	printf("Slave: Magic number = 0x%x \n", received_MagicNumber);	// REMOVE
-	printf("Slave: My RID = %d \n", received_myRID);
+	printf("Slave: My RID = %i \n", received_myRID);
 	printf("Slave: Next Slave IP Address = %s \n", nextSlaveIP_String);
-	
-    freeaddrinfo(servinfo);		// Frees up the linked list pointed to by servinfo 
-    close(sockfd);				// Close the socket that we were using 
-    
+
+    freeaddrinfo(servinfo);		// Frees up the linked list pointed to by servinfo
+    printf("Closing socket...");
+    close(sockfd);				// Close the socket that we were using
+
     return 0;
 }
